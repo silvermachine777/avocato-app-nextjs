47b1fef2bd5a41ba9447934789453fb8ab57e6df
silvermachine777 <sebastian_gg@outlook.com>
2023-04-19 09:38:59 -0500

    Corrigiendo bug de render

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,8 @@ import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
 
-export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch('https://platzi-avo.vercel.app/api/avo')
+export const getServerSideProps = async () => {
+  const response = await fetch('https://avocato-app-nextjs.vercel.app/api/avo')
   const { data: productList }: TAPIAvoResponse = await response.json()
 
   return {
